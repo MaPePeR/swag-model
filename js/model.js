31cd81241792f494621fe93e06fc5b3c6bc3c08d
MaPePeR <MaPePeR@users.noreply.github.com>
2021-09-09 22:44:54 +0200

    Model serialize and deserialize

--- js/model.js
+++ js/model.js
@@ -19,6 +19,151 @@ var model = (function () {
             this.globalBetaPoints = [];
         }
 
+        serialize() {
+            const combinedNames = [].concat(
+                this.infectionTypes.map(i => i.name),
+                this.backgrounds.map(i => i.name)
+            ).join("\0");
+            const N_I = this.infectionTypes.length;
+            const N_B = this.backgrounds.length;
+            const N_GBP = this.globalBetaPoints.length;
+            const datasize =
+                4 +                    // Version Number/Meta
+                4 +                    // Number of Infections(N_I)
+                N_I * (4 + 4) +        // Beta + Gamma
+                4 +                    // Number of Backgrounds(N_B)
+                4 * N_I * N_B * N_B +  // Beta multipliers
+                4 * N_I * N_B * N_B +  // Transititios
+                4 * (N_I + 1) * N_B +  // Start condition
+                4 +                    // Number of GlobalbetaPoints (N_GBP)
+                N_GBP * (4 + 4) +      // Global Beta Points x, y
+                4 +                    // Number of 4 Byte chars for 0 separated Names
+                combinedNames.length * 4;
+            const buffer = new ArrayBuffer(datasize);
+            const int32view = new Int32Array(buffer);
+            const float32view = new Float32Array(buffer);
+            let offset = 0;
+
+            int32view[offset++] = 1;
+            int32view[offset++] = N_I;
+            for (const infection of this.infectionTypes) {
+                float32view[offset++] = infection.beta;
+                float32view[offset++] = infection.gamma;
+            }
+            int32view[offset++] = N_B;
+            for (const infectionTypeNumber in this.infectionTypes) {
+                float32view.set(this.betaMultipliers[infectionTypeNumber].array, offset);
+                offset += N_B * N_B;
+            }
+            for (const infectionTypeNumber in this.infectionTypes) {
+                float32view.set(this.transitions[infectionTypeNumber].array, offset);
+                offset += N_B * N_B;
+            }
+            float32view.set(this.startConditon.array, offset);
+            offset += ((N_I + 1) * N_B);
+            int32view[offset++] = N_GBP;
+            for (var globalBetaPoint of this.globalBetaPoints) {
+                int32view[offset++] = globalBetaPoint.x;
+                float32view[offset++] = globalBetaPoint.y;
+            }
+            int32view[offset++] = combinedNames.length;
+            for (let i = 0; i < combinedNames.length; ++i) {
+                int32view[offset++] = combinedNames.codePointAt(i);
+            }
+
+            if (offset * 4 !== datasize) {
+                throw "something wrong with serilization";
+            }
+
+            return new Promise(resolve => {
+                const blob = new Blob([new Uint8Array(buffer)]);
+                const reader = new FileReader();
+                reader.onload = (event) => {
+                    const result = reader.result;
+                    if (result.substr(0, 37) == "data:application/octet-stream;base64,") {
+                        resolve(result.substr(37));
+                    } else {
+                        resolve(null);
+                    }
+                };
+                reader.readAsDataURL(blob);
+            });
+        }
+
+        deserializeB64(b64) {
+            const url = "data:application/octet-stream;base64," + b64;
+            fetch(url)
+                .then(res => res.arrayBuffer())
+                .then(this.deserializeBuffer.bind(this));
+        }
+
+        deserializeBuffer(buffer) {
+            const int32view = new Int32Array(buffer);
+            const float32view = new Float32Array(buffer);
+            let offset = 0;
+
+            const version = int32view[offset++];
+            if (version != 1) {
+                throw "Unknown version";
+            }
+            const N_I = int32view[offset++];
+            this.infectionTypes = [];
+            for (let i = 0; i < N_I; ++i) {
+                this.infectionTypes.push({
+                    number: i,
+                    beta: float32view[offset++],
+                    gamma: float32view[offset++],
+                });
+            }
+            const N_B = int32view[offset++];
+            this.backgrounds = [];
+            for (let i = 0; i < N_B; ++i) {
+                this.backgrounds.push({number: i});
+            }
+            this.betaMultipliers = [];
+            for (const _ in this.infectionTypes) {
+                const m = new Matrix(N_B, N_B);
+                m.array.set(float32view.subarray(offset, offset + N_B * N_B));
+                this.betaMultipliers.push(m);
+                offset += N_B * N_B;
+            }
+            this.transitions = [];
+            for (const _ in this.infectionTypes) {
+                const m = new Matrix(N_B, N_B);
+                m.array.set(float32view.subarray(offset, offset + N_B * N_B));
+                this.transitions.push(m);
+                offset += N_B * N_B;
+            }
+            this.startConditon = new Matrix(N_B, N_I + 1);
+            this.startConditon.array.set(float32view.subarray(offset, offset + (N_I + 1) * N_B));
+            offset += ((N_I + 1) * N_B);
+            const N_GBP = int32view[offset++];
+            this.globalBetaPoints = [];
+            for (let i = 0; i < N_GBP; ++i) {
+                this.globalBetaPoints.push({
+                    x: int32view[offset++],
+                    y: float32view[offset++],
+                });
+            }
+            const N_names = int32view[offset++];
+            let names = [];
+            for (let i = 0; i < N_names; i++) {
+                names.push(String.fromCodePoint(int32view[offset++]));
+            }
+            names = names.join('').split("\0");
+            let i = 0;
+            for (; i < N_I; ++i) {
+                this.infectionTypes[i].name = names[i];
+            }
+            for (; i < N_B + N_I; ++i) {
+                this.backgrounds[i  - N_I].name = names[i];
+            }
+
+            ui.recreateInfectionTypeTable();
+            ui.recreateBackgroundTable();
+        }
+
+
         createInfectionType(number, data) {
             return {
                 name: data.name,
