9edd4bcb6c25f9e02e33944b988532e9e5cff93f
MaPePeR <MaPePeR@users.noreply.github.com>
2021-09-09 20:36:53 +0200

    Additional line plot of simulation result

--- js/plot.js
+++ js/plot.js
@@ -138,28 +138,52 @@ var plot = (function () {
     class Plot {
         constructor() {
             this.EditableLinePlot = EditableLinePlot;
+
+            this.margin = {top: 20, right: 30, bottom: 30, left: 55};
+            this.width = 800;
+            this.height = 400;
+        }
+
+        makeSVG(id) {
+            return d3.select(id)
+                .append("svg")
+                    .attr("width", '100%'/*width + margin.left + margin.right*/)
+                    .attr("height", this.height + this.margin.top + this.margin.bottom)
+                    .attr('viewBox', [0, 0, this.width + this.margin.left + this.margin.right, this.height + this.margin.top + this.margin.bottom])
+                .append("g")
+                    .attr("transform", `translate(${this.margin.left}, ${this.margin.top})`);
+        }
+        makeXaxis(svg, xdomain) {
+            const x = d3.scaleLinear()
+                .domain(xdomain)
+                .range([ 0, this.width ]);
+            svg.append("g")
+                .attr("transform", `translate(0, ${this.height})`)
+                .call(d3.axisBottom(x).ticks(5));
+            return x;
+        }
+        makeYaxis(svg, ydomain) {
+            const y = d3.scaleLinear()
+                .domain(ydomain)
+                .range([ this.height, 0 ]);
+            svg.append("g")
+                .call(d3.axisLeft(y));
+            return y;
         }
         plot(data) {
             document.getElementById('plot1').innerHTML = '';
+            this.plotArea(data);
+            this.plotLines(data);
+        }
+        plotArea(data) {
             const infections = model.getInfectionTypes();
             const backgrounds = model.getBackgrounds();
 
             const timestepsize =  (infections.length + 1) * backgrounds.length;
             const timesteps = data.length / timestepsize;
 
-            const margin = {top: 20, right: 30, bottom: 30, left: 55},
-                width = 800 - margin.left - margin.right,
-                height = 400 - margin.top - margin.bottom;
-
             // append the svg object to the body of the page
-            const svg = d3.select("#plot1")
-                .append("svg")
-                    .attr("width", '100%'/*width + margin.left + margin.right*/)
-                    .attr("height", height + margin.top + margin.bottom)
-                    .attr('viewBox', [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom])
-                .append("g")
-                    .attr("transform",
-                        `translate(${margin.left}, ${margin.top})`);
+            const svg = this.makeSVG('#plot1');
 
             // List of groups = header of the csv files
             const keys = d3.range(timestepsize);
@@ -174,28 +198,17 @@ var plot = (function () {
                     }
                 )(d3.range(0, timesteps));
 
-            // Add X axis
-            const x = d3.scaleLinear()
-                .domain([0, timesteps])
-                .range([ 0, width ]);
-            svg.append("g")
-                .attr("transform", `translate(0, ${height})`)
-                .call(d3.axisBottom(x).ticks(5));
+            const x = this.makeXaxis(svg, [0, timesteps]);
 
             const max = d3.max(stackedData, d => d3.max(d, d => d[1]));
-            // Add Y axis
-            const y = d3.scaleLinear()
-                .domain([0, max])
-                .range([ height, 0 ]);
-            svg.append("g")
-                .call(d3.axisLeft(y));
+
 
             // color palette
             const color = d3.scaleOrdinal()
                 .domain(keys)
                 .range(d3.schemeCategory10);
 
-
+            const y = this.makeYaxis(svg, [0, max]);
 
             // Show the areas
             svg
@@ -214,6 +227,43 @@ var plot = (function () {
                         return backgrounds[bg].name + " " + (inf == 0 ? "Uninfected" : infections[inf - 1].name);
                     });
         }
+
+        plotLines(data) {
+            const infections = model.getInfectionTypes();
+            const backgrounds = model.getBackgrounds();
+
+            const timestepsize =  (infections.length + 1) * backgrounds.length;
+            const timesteps = data.length / timestepsize;
+
+            const svg = this.makeSVG('#plot1');
+
+            const max = d3.max(data);
+
+            const x = this.makeXaxis(svg, [0, timesteps]);
+            const y = this.makeYaxis(svg, [0, max]);
+
+            const keys = d3.range(timestepsize);
+
+            // color palette
+            const color = d3.scaleOrdinal()
+                .domain(keys)
+                .range(d3.schemeCategory10);
+
+            const xvalues = d3.range(timesteps);
+
+            svg.append('g')
+                .selectAll("path")
+                .data(keys)
+                .join('path')
+                    .attr("fill", "none")
+                    .attr("stroke-width", 1)
+                    .attr("stroke", d => color(d))
+                    .attr("d", series => (d3.line()
+                            .x(d => x(d))
+                            .y(d => y(data[d * timestepsize + series]))(xvalues))
+                    );
+        }
+
     }
 
     return new Plot();
